521f8e29fdb3da05b0f5793e8a000b0049ade4d2
Liang Jia Sheng <liangsheng4@gmail.com>
2017-03-05 18:02:02 +0800

    add compression import

--- index.js
+++ index.js
@@ -3,10 +3,10 @@
  */
 var express = require('express');
 var path = require('path');
+var compression = require('compression')
 var app = express();
 
 app.use('/', express.static('./'));
 app.use(compression())
-
 
 app.listen(8080);
