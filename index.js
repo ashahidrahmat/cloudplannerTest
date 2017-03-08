ea6982a6c46c4c88d14fefb9b2bd5da980f6b213
Liang Jia Sheng <liangjs@uradevt.gov.sg>
2017-03-08 17:21:59 +0800

    test compression

--- index.js
+++ index.js
@@ -6,7 +6,11 @@ var path = require('path');
 var compression = require('compression')
 var app = express();
 
+app.use(compression({ threshold: 0 }));
 app.use('/', express.static('./'));
-app.use(compression())
 
-app.listen(8080);
+var port = process.env.PORT || 3000;
+
+var server = app.listen(port, function () {
+	console.log('Server running at http://127.0.0.1:' + port + '/');
+});
